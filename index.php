dddf02b809fbc03fd98d8cd58592fdf1def38563
James Acklin <james@acklindesign.com>
2014-10-27 21:56:00 -0400

    Stores custom post type, loop on homepage

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
   $context = Timber::get_context();
   $context['page'] = Timber::get_post();
+  $context['stores'] = Timber::get_posts('post_type=store');
   $templates = array('index.twig');
   if (is_front_page()){
     array_unshift($templates, 'home.twig');
